6eb1d29734a66d1b7f0622fc7f43d513e8b5dc43
SzegedAndor <103267438+SzegedAndor@users.noreply.github.com>
2022-04-08 17:21:13 +0200

    Add files via upload

---
+++ pythonkezdes6.py
@@ -0,0 +1,14 @@
+for i in range(0,101):
+    
+    if i % 3 == 0 and i % 5 == 0:
+        print("Viktorbuzi")
+    elif i % 3 == 0:
+        print("fiz")
+    elif i % 5 == 0:
+        print("buzi")
+    elif i % 3 != 0:
+        print(i)
+    elif i % 5 != 0:
+        print(i)
+    
+        
